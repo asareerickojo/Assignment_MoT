41712eba223e8cbb6ee69d436626ac4394bb6275
unique-2018 <asare.eric.asare@gmail.com>
2023-01-07 11:21:46 -0500

    updated and integrated functions into one function

---
+++ R/old_functions/validation.R
@@ -0,0 +1,12 @@
+#' Sum of vector elements
+#'
+#' @description
+#' `sum` returns the sum of all the values present in its arguments.
+#' @details
+#' This is a generic function: methods can be defined for it directly
+#' or via the [Summary()] group generic. For this to work properly,
+#' the arguments `...` should be unnamed, and dispatch is on the
+#' first argument
+#' @param
+#' @return describes the output from the function. Briefly describe the type/shape of the output
+#' @examples The description should provide a succinct summary of parameter type (e.g. a string, a numeric vector)
